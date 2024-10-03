af7888d7be1381535a37fcc78fd941cf266c5c56
Vitor Katakura <7vitorgomez@gmail.com]>
2024-10-03 13:32:21 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,13 @@
 "use client";
 
-import {
-  Carousel,
-  CarouselApi,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [api, setApi] = useState<CarouselApi>();
-  const [current, setCurrent] = useState(0);
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    if (!api) {
-      return;
-    }
-
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1);
-    });
-  }, [api]);
-
   return (
     <div className="w-screen flex justify-center items-center h-screen flex-col gap-4">
       <h1 className="font-bold text-4xl">у спогадах Феліпе Баковського</h1>
-      <Carousel setApi={setApi} className="mx-auto max-w-md">
+      <Carousel className="mx-auto max-w-md">
         <CarouselContent>
           <CarouselItem>
             <Image src={"/b.jpeg"} alt="B" width={500} height={500} />
